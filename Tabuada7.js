0168ecfcca2c85cb6b2040d84dfeaebc4f9ea2ed
Israel <israelmelo77@gmail.com>
2026-07-29 13:57:39 -0300

    Estudo na Pratica.

---
+++ Tabuada7.js
@@ -0,0 +1,8 @@
+// tabuada-dinamica.js
+const numero = 7; // Mude aqui ou use prompt com biblioteca
+
+console.log(`===== TABUADA DO ${numero} =====`);
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}
