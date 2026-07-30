cb1111020eff7f29eafaf91e74e0421c8d45c5b9
Israel <israelmelo77@gmail.com>
2026-07-30 07:32:41 -0300

    Estudo na Pratica, Função e Tabuada.

--- Tabuada7.js
+++ Tabuada7.js
@@ -1,8 +1,22 @@
-// tabuada-dinamica.js
+/* tabuada-dinamica.js
 const numero = 7; // Mude aqui ou use prompt com biblioteca
 
 console.log(`===== TABUADA DO ${numero} =====`);
 
-for (let i = 0; i <= 5; i++) {
+for (let i = 0; i <= 10; i++) {
     console.log(`${numero} x ${i} = ${numero * i}`);
+} */
+
+function tabuada(numero) {
+    let resultado = `===== TABUADA DE ${numero} =====\n`;
+
+    for (let i = 0; i <= 10; i++) { // 1. Começa em 0  | 2. Vai até <= 10
+        resultado += `${numero} x ${i} = ${numero * i}\n`;
+    }
+
+    resultado += "===========================";
+    return resultado;
 }
+
+// Testando
+console.log(tabuada(7));
